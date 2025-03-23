bef31fac1bad1d5f3f47c2230bf00d8b939a2e25
Shivaram Karthikeyan <shivaramkpk@gmail.com>
2025-03-23 19:44:05 +0530

    folder str

---
+++ code/src/Backend/utilities/llm.py
@@ -0,0 +1,49 @@
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+
+def get_ai_explanation(model_name="gemini-2.0-flash", email="Explain how AI works"):
+    
+    load_dotenv()  
+    api_key = os.getenv("GENAI_API_KEY")
+
+    if not api_key:
+        print("Error: GENAI_API_KEY not found in environment variables.")
+        return None
+    prompt = f"""Analyse the following email and give me the outputs in the following json format
+    
+    -   Summary: A concise summary of the email's content.
+    -   Request code : Request code the emailc an be classified into.
+    -   Sub-request Code: [list of strings]. The possible sub request types the email can be classified into  
+    -   Sender Address: The sender's address, if provided.
+    -   Primary Request: The main request for the email.
+    -   Key Values: Any key points mentioned in the email in a json format. Eg: {"key1": "value1", "key2": "value2"}
+
+    Email Description:
+    {email}
+
+    Output in JSON format:
+    {{
+        "summary": "",
+        "request_type": "",
+        "sub_request_type": "",
+        "sender_address": "",
+        "primary_request: "",
+        "key_values": keypoints in json format.
+    }}
+    """
+    try:
+        genai.configure(api_key=api_key) 
+        model = genai.GenerativeModel(model_name)
+        response = model.generate_content(prompt)
+        return response.text
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+if __name__ == "__main__":
+    emails="""
+    """
+    explanation = get_ai_explanation(email=emails)
+    if explanation:
+        print(explanation)
